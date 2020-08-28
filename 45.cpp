69a2cf61d09ce781ec7ee74a8097dfbb27311873
ANI3792 <57237655+ANI3792@users.noreply.github.com>
2020-08-28 12:59:28 +0530

    Add files via upload

---
+++ 45.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int che[288]={0};
+	string st="opposite";
+	for(int i=0;i<st.size();i++)
+	che[st[i]]++;
+	for(int i=0;i<st.size();i++)
+	cout<<che[st[i]]<<endl;
+	
+	cout<<che['o']<<endl;
+	return 0;
+}
